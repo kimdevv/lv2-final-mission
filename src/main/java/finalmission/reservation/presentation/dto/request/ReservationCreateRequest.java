6a4219bda1be0675a0705c1decbf51c3be2e109c
kimdev <kim__dev@hufs.ac.kr>
2025-06-10 17:25:51 +0900

    feat: 예약 생성 요청 Dto 검증 로직 추가

--- src/main/java/finalmission/reservation/presentation/dto/request/ReservationCreateRequest.java
+++ src/main/java/finalmission/reservation/presentation/dto/request/ReservationCreateRequest.java
@@ -5,4 +5,19 @@ import finalmission.medical.model.TreatmentType;
 import java.time.LocalDate;
 
 public record ReservationCreateRequest(TreatmentType treatmentType, LocalDate date, Long timeId, String name) {
+
+    public ReservationCreateRequest {
+        if (treatmentType == null) {
+            throw new IllegalArgumentException("진료 종류는 null이 될 수 없습니다.");
+        }
+        if (date == null) {
+            throw new IllegalArgumentException("예약 날짜는 null이 될 수 없습니다.");
+        }
+        if (timeId == null) {
+            throw new IllegalArgumentException("예약 시간은 null이 될 수 없습니다.");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("예약자는 null이 될 수 없습니다.");
+        }
+    }
 }
