a7ce9398965cd577f3cc79e5bf394a95ccb67704
kimdev <kim__dev@hufs.ac.kr>
2025-06-10 15:11:57 +0900

    feat: Resrvation, Time, TreatmentType 엔티티 생성

---
+++ src/main/java/finalmission/reservation/model/Reservation.java
@@ -0,0 +1,66 @@
+package finalmission.reservation.model;
+
+import finalmission.medical.model.TreatmentType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TreatmentType treatmentType;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private Time time;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Reservation() {}
+
+    public Reservation(TreatmentType treatmentType, LocalDate date, Time time, String name) {
+        this.id = null;
+        this.treatmentType = treatmentType;
+        this.date = date;
+        this.time = time;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public TreatmentType getTreatmentType() {
+        return treatmentType;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
