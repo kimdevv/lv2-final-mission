10ead56100adc0e8f117720046f731d13da9f29d
kimdev <kim__dev@hufs.ac.kr>
2025-06-10 13:26:43 +0900

    feat: 휴일을 저장하는 Holiday 엔티티 생성

---
+++ src/main/java/finalmission/holiday/model/Holiday.java
@@ -0,0 +1,23 @@
+package finalmission.holiday.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Holiday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private String name;
+}
